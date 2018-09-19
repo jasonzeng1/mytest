9ab09c1ce0a8adac45e693d8cf39be6bdbb55660
zr <zengrui2008163@163.com>
2018-09-19 16:54:55 +0800

    change

--- git_test_demoprj1/src/main/java/com/itheima/app/AppRun.java
+++ git_test_demoprj1/src/main/java/com/itheima/app/AppRun.java
@@ -4,5 +4,6 @@ public class AppRun {
     public static void main(String[] args) {
         System.out.println("hello java!");
 		System.out.println("hello java!");
+		System.out.println("hello github!");
     }
 }
