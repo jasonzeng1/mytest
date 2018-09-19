4cc9549a11150fa4e8938895c7c1dfc05b2f8b2e
zr <zengrui2008163@163.com>
2018-09-19 15:46:04 +0800

    修改

--- git_test_demoprj1/src/main/java/com/itheima/app/AppRun.java
+++ git_test_demoprj1/src/main/java/com/itheima/app/AppRun.java
@@ -3,5 +3,6 @@ package com.itheima.app;
 public class AppRun {
     public static void main(String[] args) {
         System.out.println("hello java!");
+		System.out.println("hello java!");
     }
 }
